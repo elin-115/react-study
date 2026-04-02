f2a17642c22cf33915fb3a781268e28c9cf190c8
elin-115 <j.yeon9919@gmail.com>
2026-04-02 16:50:39 +0900

    git error

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
-
-// 현재 빌드 모드가 'gh-pages'인지 확인하는 변수입니다.
-const isGitHubPages = import.meta.env.MODE === 'gh-pages';
-
-// GitHub Pages일 때는 '/react-study'를, 그 외(Vercel 등)에는 빈 값을 사용합니다.
-const baseName = isGitHubPages ? '/react-study' : '';
-
 createRoot(document.getElementById('root')).render(
     
-    <BrowserRouter basename={baseName}>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  // 배포 명령어가 gh-pages일 때만 /react-study/ 적용
-  const isGitHubPages = mode === 'gh-pages';
-
-  return {
-    plugins: [react()],
-    base: isGitHubPages ? '/react-study/' : '/',
-  }
-})
+export default defineConfig({
+  plugins: [react()],
+  base: "",
+});
