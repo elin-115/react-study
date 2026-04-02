8d672671e383d766d1d7f3ddc6d9eadd7132a1e1
elin-115 <j.yeon9919@gmail.com>
2026-04-02 15:46:01 +0900

    git & vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  // Vercel 배포 시에는 "/", GitHub Pages 배포 시에는 "/react-study/" 적용
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL 
+    ? '/react-study/' 
+    : '/',
 })
