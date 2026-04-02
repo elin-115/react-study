19c64606eb7d6a925928e4df25d4249136b38f73
elin-115 <j.yeon9919@gmail.com>
2026-04-02 16:03:24 +0900

    main basename

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
+// Vercel 환경(process.env.VERCEL)일 때는 basename을 없애고, 아니면 gh-pages용 경로 사용
+const basename = import.meta.env.MODE === 'gh-pages' ? '/react-study' : '';
+
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename="/react-study">
+    
+    <BrowserRouter basename={basename}>
         <App />
     </BrowserRouter>
 )
